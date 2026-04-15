6a9e12a2246c341273eb253760059c3ca78c6f7b
Paikarthik <karthikpai2002@gmail.com>
2026-04-16 00:11:19 +0530

    Function to get computer choice
    -Added function to get computer's choice by using random module

--- src/rock_paper_scissors.js
+++ src/rock_paper_scissors.js
@@ -1,1 +1,22 @@
-console.log("Hello World")
+
+
+function getComputerChoice(){
+
+    let random_number = Math.random();
+
+    if (random_number <=0.33){
+        return "Rock"
+    }
+
+    else if(random_number>0.33 && random_number<=0.66){
+        return "Paper"
+    }
+
+    else{
+        return "Scissors"
+    }
+
+}
+
+
+
